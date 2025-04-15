2dca59be73606b54eaabace47a9b0c304c96b55e
Leonardo <leonardorscarpitta@gmail.com>
2025-04-15 17:43:48 -0300

    feat: create a number validation

--- src/main/java/io/github/leonardorscarpitta/simplify/utils/domain/ExceptionHandling.java
+++ src/main/java/io/github/leonardorscarpitta/simplify/utils/domain/ExceptionHandling.java
@@ -1,16 +1,17 @@
 package io.github.leonardorscarpitta.simplify.utils.domain;
 
+import io.github.leonardorscarpitta.simplify.utils.exceptions.InvalidValue;
+
 public class ExceptionHandling {
-    public static <T> Boolean checkForNan(T value) {
+    public static <T> void checkForNan(T value) {
         String convertedValue = (String) value;
 
         for (int i = 0; i < convertedValue.length(); i++) {
             boolean notNumber = !Character.isDigit(convertedValue.charAt(i));
             if (notNumber) {
-                return false;
+                System.out.printf("Dígito: %s", i);
+                throw new InvalidValue("O valor passado deve ser um número!");
             }
         }
-
-        return true;
     }
 }
