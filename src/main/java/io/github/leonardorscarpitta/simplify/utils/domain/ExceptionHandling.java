ca28d0f2787f2c8eef4f68f2a8be2683ca719212
Leonardo <leonardorscarpitta@gmail.com>
2025-04-17 19:17:16 -0300

    feat: create verification to check if the number is null and set exception class as a ResponseStatus

--- src/main/java/io/github/leonardorscarpitta/simplify/utils/domain/ExceptionHandling.java
+++ src/main/java/io/github/leonardorscarpitta/simplify/utils/domain/ExceptionHandling.java
@@ -4,12 +4,15 @@ import io.github.leonardorscarpitta.simplify.utils.exceptions.InvalidValue;
 
 public class ExceptionHandling {
     public static <T> void checkForNan(T value) {
-        String convertedValue = (String) value;
+        String convertedValue = String.valueOf(value);
+
+        if (convertedValue == null) {
+            throw new InvalidValue("O valor não pode ser nulo!");
+        }
 
         for (int i = 0; i < convertedValue.length(); i++) {
             boolean notNumber = !Character.isDigit(convertedValue.charAt(i));
             if (notNumber) {
-                System.out.printf("Dígito: %s", i);
                 throw new InvalidValue("O valor passado deve ser um número!");
             }
         }
--- src/main/java/io/github/leonardorscarpitta/simplify/utils/exceptions/InvalidValue.java
+++ src/main/java/io/github/leonardorscarpitta/simplify/utils/exceptions/InvalidValue.java
@@ -1,5 +1,9 @@
 package io.github.leonardorscarpitta.simplify.utils.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidValue extends RuntimeException {
     public InvalidValue(String message) {
         super(message);
