9909c3e042efb4f706d78da971ee995e5461ba21
Leonardo <leonardorscarpitta@gmail.com>
2025-04-17 10:56:28 -0300

    refactor: change strategy type for generated id value

--- src/main/java/io/github/leonardorscarpitta/simplify/models/ToDoItem.java
+++ src/main/java/io/github/leonardorscarpitta/simplify/models/ToDoItem.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Table(name = "tasks")
 public class ToDoItem {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
