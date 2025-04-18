8d3618d932ecc201d9dd416d8687b2f57aa7c549
Leonardo <leonardorscarpitta@gmail.com>
2025-04-18 17:45:55 -0300

    chore: remove unused imports

--- src/main/java/io/github/leonardorscarpitta/simplify/config/SwaggerConfig.java
+++ src/main/java/io/github/leonardorscarpitta/simplify/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package io.github.leonardorscarpitta.simplify.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
